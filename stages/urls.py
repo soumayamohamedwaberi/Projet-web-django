2c1871c96a866949753bfbd50f48c030ca184912
elfazzazi-ilyass <elfazzazi.ilyass@etu.uae.ac.ma>
2026-01-14 17:07:22 +0100

    Configure URL routing for stages app

---
+++ stages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.liste_offres, name='liste_offres'),
+    path('postuler/<int:offre_id>/', views.postuler, name='postuler'),
+    path('mes-candidatures/', views.mes_candidatures, name='mes_candidatures'),
+]
