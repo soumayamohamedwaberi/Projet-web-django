74c5b5c8e7a0dd7076cbb17575aebf04fb491c55
linarached <lina.rached@etu.uae.ac.ma>
2026-01-17 11:12:26 +0100

    Update urls.py

--- stages/urls.py
+++ stages/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.liste_offres, name='liste_offres'),
+    path('<int:pk>/', views.detail_offre, name='detail_offre'),
+    path('creer/', views.creer_offre, name='creer_offre'),
+    path('<int:pk>/modifier/', views.modifier_offre, name='modifier_offre'),
+]
 
