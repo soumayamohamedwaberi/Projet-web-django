b8e2653545909e7eb08ecf3b250e60f5749206fb
elfazzazi-ilyass <elfazzazi.ilyass@etu.uae.ac.ma>
2026-01-18 18:41:19 +0100

    Design: Ajout du style Bootstrap pour la liste et le formulaire

--- Mon_site/urls.py
+++ Mon_site/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stages/', include('stages.urls')),
+    path('', include('stages.urls')),
 ]
--- stages/admin.py
+++ stages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import OffreStage, Candidature
 
+admin.site.register(OffreStage)
+admin.site.register(Candidature)
 # Register your models here.
