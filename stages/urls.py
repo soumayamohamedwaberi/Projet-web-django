31d15cfb6b64df413a44db175e009db0dd765d45
soumayamohamedwaberi <soumayamohamedwaberi@gmail.com>
2026-01-18 16:55:48 +0300

    Update urls.py

--- stages/urls.py
+++ stages/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.liste_offres, name='liste_offres'),
+    path('postuler/<int:offre_id>/', views.postuler, name='postuler'),
     path('<int:pk>/', views.detail_offre, name='detail_offre'),
     path('creer/', views.creer_offre, name='creer_offre'),
     path('<int:pk>/modifier/', views.modifier_offre, name='modifier_offre'),
