566e5b3bf93a48d31e05014d07e1a392f8d5f93c
elfazzazi-ilyass <elfazzazi.ilyass@etu.uae.ac.ma>
2026-01-19 18:38:24 +0100

    Ajout: Connexion étudiant + Statut candidature + Suppression

--- Mon_site/urls.py
+++ Mon_site/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stages/', include('stages.urls')), 
-    path('', include('stages.urls')),
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
